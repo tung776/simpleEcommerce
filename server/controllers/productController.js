455e5a5e9d09e607bced4fe5b886f68ee030ac62
nguyen thanh tung <thanhtung776@gmail.com>
2017-05-16 19:43:34 -0700

    fix some code

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,57 +2,27 @@ const productController = {};
 import  categoryModel from "../models/categoryModel";
 import productmodel from "../models/productModel";
 
-// productmodel.createMapping(function(err, mapping){
-//     if(err){
-//         console.log("mapping has error");
-//         console.log(err);
-//     }
-//     else{
-//         console.log("mapping success");
-//         //console.log(mapping);
-//     }
-// });
-
-// const stream = productmodel.synchronize();
-// let count = 0;
-// stream.on("error", function(err){
-//     console.log(err);
-// });
-
-// stream.on("data", function(data){
-//     count ++;
-// });
-
-// stream.on("close", function(){
-//     console.log("maps " + count + " documents");
-// });
-
-// productController.getSearch = function(req, res, next){
-//     console.log("go here");
-//     res.send("hello");
-//     // if(req.query.q){
-//     //     productmodel.search({
-//     //         query_string: {query: req.query.q}
-//     //     }, function(err, results){
-//     //         if(err) return next(err);
-            
-//     //         const data = results.hits.hits.map(function(hit){
-//     //             return hit;
-//     //         });
-//     //         res.render('products/search', {
-//     //             query: req.query.q,
-//     //             data: data
-//     //         });
-//     //     });
-//     // }
-// };
-
 productController.postSearch = async function(req, res, next){
+    try {
     const search = req.body.search;
     const regExp = new RegExp(req.body.search, "i");
     const query = {name: regExp}; 
-    const foundProduct = await productmodel.find(query).populate("category");
-    res.render("products/products", { products: foundProduct });
+     let perpage = 9;
+        let page = req.query.page || 1;
+    const foundProduct = await productmodel.find(query)
+                                        .limit(perpage )
+                                        
+                                        .populate("category");
+    let count = await productmodel.count(query);
+    res.render("products/products", { 
+        products: foundProduct,
+        pages: Math.floor(count/perpage)
+    });
+    }
+    catch(err){
+        console.log(err);
+        next(err);
+    }
 };
 
 
@@ -80,12 +50,22 @@ productController.getProducts = async function (req, res, next){
 
 productController.getProductByCategoryId = async (req, res, next)=> {
     try {
+        let perpage = 9;
+        let page = req.query.page || 1;
         let categoryId = req.params.categoryId; 
-        const products = await productmodel.find({category: categoryId});
-        res.render("products/products", {products: products});
+        const products = await productmodel.find({category: categoryId})
+                                            .limit(perpage )
+                                            .skip( perpage * page)
+                                            .populate("category");
+        let count = await productmodel.count({category: category._id});
+        res.render("products/products", {
+            products: products,
+            pages: Math.floor(count/perpage)
+        });
     }
     catch(err){
         console.log(err);
+        next(err);
     }
   
 };
@@ -99,6 +79,7 @@ productController.getDetailProduct = async (req, res, next)=> {
     }
     catch(err){
         console.log(err);
+        next(err);
     }
 };
 export default productController;
